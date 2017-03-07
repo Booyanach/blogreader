fc6fe9b505cec31fe98c9ee9f08b14f06ffdb04a
Booyanach <sandwave@gmail.com>
2017-03-07 17:11:50 +0000

    State service is more complete.
    Makes calls to the API now.
    Has helper functions to select items, call the Api, and get items.
    
    Added dep on app.component so all sub-components can use it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { StateService } from './common/state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StateService]
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/common/state.service.ts
+++ src/app/common/state.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Item } from './item';
 
 @Injectable()
 export class StateService {
 
-  constructor() { }
+  selectedItem: Item;
+  state: Item[];
+  url: String = 'http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: Http) {
+  }
+
+  getState(): Promise<Item[]> {
+    return Promise.resolve(this.callApi(
+      this.url,
+      response => this.state = response.json() as Item[]
+    ));
+  }
+
+  getSelectedItem(): Promise<Item> {
+    return Promise.resolve(this.selectedItem);
+  }
+
+  selectItem(item: Item) {
+    this.selectedItem = item;
+  }
+
+  private callApi(url: String, success: any): Promise<any> {
+    return this.http.get(<string>url)
+          .toPromise()
+          .then(success)
+          .catch(this.errorHandler);
+  }
+
+  errorHandler(e: any): Promise<any> {
+    console.error('stateService', e);
+    return Promise.reject(e.message || e);
+  }
 
 }
