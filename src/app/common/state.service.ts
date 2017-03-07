13ae6a3b10099537aafc0578c7279359cb23d86d
Booyanach <sandwave@gmail.com>
2017-03-07 19:02:51 +0000

    selectedItem is now an observable.
    gets updated when a click is handled in the list.

--- src/app/common/state.service.ts
+++ src/app/common/state.service.ts
@@ -1,33 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/observable/of';
 
 import { Item } from './item';
 
 @Injectable()
 export class StateService {
 
-  selectedItem: Item;
+  selectedItem: Observable<Item>;
+  observer: Observer<Item>;
   state: Item[];
   url: String = 'http://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: Http) {
+    this.selectedItem = <Observable<Item>> new Observable(observer => this.observer = observer);
   }
 
   getState(): Promise<Item[]> {
     return Promise.resolve(this.callApi(
       this.url,
-      response => this.state = response.json() as Item[]
+      response => {
+        this.state = response.json() as Item[];
+        this.observer.next(this.state[0]);
+        return this.state;
+      }
     ));
   }
 
-  getSelectedItem(): Promise<Item> {
-    return Promise.resolve(this.selectedItem);
+  getSelectedItem(): Observable<Item> {
+    return this.selectedItem;
   }
 
   selectItem(item: Item) {
-    this.selectedItem = item;
+    this.observer.next(item);
   }
 
   private callApi(url: String, success: any): Promise<any> {
